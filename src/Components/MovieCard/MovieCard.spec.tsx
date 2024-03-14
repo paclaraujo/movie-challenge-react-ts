76739353eb0c42046a801d3465c5989fb3dcc253
Paloma Araujo <paclaraujo@gmail.com>
2024-03-14 08:16:20 -0300

    test: improve movie card tests

--- src/Components/MovieCard/MovieCard.spec.tsx
+++ src/Components/MovieCard/MovieCard.spec.tsx
@@ -1,33 +1,33 @@
-import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react';
-import MovieCard from './MovieCard';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import MovieCard from "./MovieCard";
 
 const movie = {
-  image: `https://media.themoviedb.org/t/p/w220_and_h330_face/kCGlIMHnOm8JPXq3rXM6c5wMxcT.jpg`,
-  title: "Poor Things",
+  image: `https://example.com/movie1.jpg`,
+  title: "Movie 1",
   releaseDate: new Date("2023-12-07"),
   originalLanguage: "en",
-  originalTitle: "Poor Things",
-  overview:  "Brought back to life by an unorthodox scientist.",
-  voteAverage: 7.901, 
-}
+  originalTitle: "Movie 1",
+  overview: "Description Movie 1",
+  voteAverage: 7.901,
+};
 
 describe("Movie Card component", () => {
   beforeEach(() => {
-    render(<MovieCard movie={movie}/>);
-  })
+    render(<MovieCard movie={movie} />);
+  });
 
-  it('renders movie poster', () => {
-    const poster = screen.queryByRole('img');
+  it("renders movie poster", () => {
+    const poster = screen.queryByRole("img");
 
     expect(poster).toHaveAttribute("src", movie.image);
   });
 
-  it('renders movie infos', () => {
-    const liItems = screen.queryAllByRole('listitem');
+  it("renders movie infos", () => {
+    const liItems = screen.queryAllByRole("listitem");
 
-    expect(liItems[0]).toHaveTextContent("Poor Things");
+    expect(liItems[0]).toHaveTextContent(movie.title);
     expect(liItems[1]).toHaveTextContent("★ 7.9");
     expect(liItems[2]).toHaveTextContent("2023");
   });
-})
+});
