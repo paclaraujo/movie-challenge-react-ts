e854ff65060902f48988ccb006384ff4f43600cb
Paloma Araujo <paclaraujo@gmail.com>
2024-03-14 09:15:07 -0300

    feat: add home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import { getMovies } from "../../services/APIService";
+import MovieList from "../../Components/MovieList/MovieList";
+import { Movie } from "../../models/Movie";
+import "./Home.css"
+
 const Home = () => {
-  return <p>Home</p>
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getMovies()
+      .then((response) => {
+        setMovies(response)
+        setIsLoading(false)
+      }).catch((err) => {
+        setError(err)
+        setIsLoading(false)
+      })
+  }, [])
+
+
+  return <>
+    {error ? <p>{error}</p> : isLoading ? <span className="loader"></span> : <MovieList movies={movies}/>}
+  </>
+  
 };
 
 export default Home;
