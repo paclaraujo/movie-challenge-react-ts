0d1af69a43f1ce015f8d6203869bfaea8b8a4dc6
Paloma Araujo <paclaraujo@gmail.com>
2024-03-14 07:44:36 -0300

    refactor: remove react import

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../Pages/Home/Home";
 import MovieDetails from "../Pages/MovieDetails/MovieDetails";
 
-
-
 export const router = createBrowserRouter([
   {
     path: "/",
