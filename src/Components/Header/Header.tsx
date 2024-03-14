4a0c645e5b0c945cffc15951d01706bef2ebd0dc
Paloma Araujo <paclaraujo@gmail.com>
2024-03-14 09:14:10 -0300

    feat: add header component

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+
+const Header = () => {
+  return <header className='header'>
+    <h1 className='header__title'>MOVIEFLIX</h1>
+    <ul className='header__itens'>
+      <li>Filtrar</li>
+      <li>Ordenar</li>
+    </ul>
+  </header>
+};
+
+export default Header;
