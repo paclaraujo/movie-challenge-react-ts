962a22f5e1ecb12da0715bdfafe3c9f3431cdcd0
Paloma Araujo <paclaraujo@gmail.com>
2024-03-14 05:49:26 -0300

    refactor: rename folders

--- src/utils/transformers.tsx
+++ src/utils/transformers.tsx
@@ -1,4 +1,4 @@
-import { Movie } from "../Models/Movie"
+import { Movie } from "../models/Movie"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const formatMovie = (movie: any) : Movie => ({
