98a3d3daa533c7000f7f170ff245c5a5e13bc8c0
Paloma Araujo <paclaraujo@gmail.com>
2024-03-19 01:38:27 -0300

    feat; add movie details view

--- src/Pages/MovieDetails/MovieDetails.tsx
+++ src/Pages/MovieDetails/MovieDetails.tsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react';
+import './MovieDetails.css';
+import { useParams } from 'react-router-dom';
+import { getMoviesDetails } from '../../services/movieService';
+import { Movie } from '../../models/Movie';
+import Spinner from '../../Components/Spinner/Spinner'
+
 const MovieDetails = () => {
-  return <p>MovieDetails</p>
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [ movie, setMovie ] = useState<Movie>()
+
+  useEffect(() => {
+    if(id) {
+      getMoviesDetails(Number(id))
+        .then((response) => {
+          setMovie(response)
+          setIsLoading(false)
+        })
+        .catch()
+    }
+  }, [id])
+  return <div>
+    <a href="/">Voltar</a>
+    {isLoading ? <Spinner /> : movie && 
+      <div className="movie_details">
+        <img className="movie_details__image" src={movie.image} />
+        <ul className="movie_details__infos">
+          <div className="movie_details__infos__container">
+            <li className="movie_details__title">{movie.title}</li>
+            <li className="movie_details__average">★ {movie.voteAverage.toFixed(1)}</li>
+          </div>
+          <li className="movie_details__title">{movie.releaseDate.getFullYear()}</li>
+          {movie.genres.map((genre, index  )=> <li key={genre+index} className="movie_details__genres">{genre}</li>)}
+          <li className="movie_details__overview">{movie.overview}</li>
+        </ul>
+      </div>
+    }
+    
+  </div>
 };
 
 export default MovieDetails;
--- src/models/Movie.d.ts
+++ src/models/Movie.d.ts
@@ -1,4 +1,5 @@
 export type Movie = {
+  id: number;
   image: string;
   title: string;
   releaseDate: Date;
